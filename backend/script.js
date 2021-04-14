850fc594aeac479046e1ad44650bb075d86c86bc
YT <hahaeating1@gmail.com>
2021-04-14 19:25:47 +0800

    first

---
+++ backend/script.js
@@ -0,0 +1,200 @@
+let generalURL = "https://hexschoollivejs.herokuapp.com/api/livejs/v1";
+let api_path = "hahaeating";
+const config = {
+	headers: {
+		Authorization: "ziorRmqHIzVkl06Sl93DJIagVHq2"
+	}
+};
+
+let orderData = [];
+let productName = {};
+let c3Data = [];
+const color1 = "#DACBFF";
+const color2 = "#9D7FEA";
+const color3 = "#5434A7";
+const color4 = "#301E5F";
+let colors = [color1, color2, color3, color4];
+let orderInfo = document.querySelector(".orderInfo > table");
+let deleteAll = document.querySelector(".orderInfo > button");
+
+//order data render
+function getOrderData() {
+	axios.get(`${generalURL}/admin/${api_path}/orders`, config)
+		.then(function (response) {
+			orderData = response.data.orders;
+			renderOrder();
+			createProductData();
+			creatC3Data();
+			renderC3();
+		});
+}
+
+//Order display
+function renderOrder() {
+	let str = "";
+	orderData.forEach((item) => {
+		let timeInMs = new Date(item.createdAt);
+		let Y = timeInMs.getFullYear() + "/";
+		let M = (timeInMs.getMonth()+1 < 10 ? "0"+(timeInMs.getMonth()+1) : timeInMs.getMonth()+1) + "/";
+		let D = timeInMs.getDate();
+
+		str += `
+		<tr class="${item.id}">
+			<td>${item.createdAt}</td>
+			<td>${item.user.name}<span>${item.user.tel}</span></td>
+			<td>${item.user.address}</td>
+			<td>${item.user.email}</td>
+			<td>${item.products[0].title}</td>
+			<td>${Y+M+D}</td>
+			<td><a class="status" data-status="${item.paid}" data-id="${item.id}">${item.paid == true ? "已處理" : "未處理" }</a></td>
+			<td><p class="delete" data-id="${item.id}">刪除</p></td>
+		</tr>
+		`;
+	});
+	orderInfo.innerHTML = `
+	<tr>
+		<th>訂單編號</th>
+		<th>聯絡人</th>
+		<th>聯絡地址</th>
+		<th>電子郵件</th>
+		<th>訂單品項</th>
+		<th>訂單日期</th>
+		<th>訂單狀態</th>
+		<th>操作</th>
+	</tr>` + str;
+}
+
+// change status + delete order
+orderInfo.addEventListener("click", function(e) {
+	e.preventDefault();
+	const targetClass = e.target.getAttribute("class");
+
+	if (targetClass == "delete") {
+		let id = e.target.getAttribute("data-id");
+		deleteOrder(id);
+		return;
+	}
+
+	if (targetClass == "status") {
+		let status = e.target.getAttribute("data-status");
+		let id = e.target.getAttribute("data-id");
+		changeStatus(status, id)
+		return;
+	}
+});
+
+function changeStatus(status, id) {
+	let newStatus;
+	status == "true" ? newStatus = false : newStatus = true;
+	axios.put(`${generalURL}/admin/${api_path}/orders`,
+		{
+			"data": {
+				"id": id,
+				"paid": newStatus
+			}}, config)
+		.then(function () {
+			alert("已更改訂單狀態");
+			getOrderData();
+		});
+}
+
+//delete an order
+function deleteOrder(id) {
+	axios.delete(`${generalURL}/admin/${api_path}/orders/${id}`, config)
+		.then(function () {
+			alert("已刪除一筆訂單");
+			getOrderData();
+		});
+}
+
+//delete all orders
+deleteAll.addEventListener("click", function() {
+	axios.delete(`${generalURL}/admin/${api_path}/orders`, config)
+		.then(function () {
+			alert("已刪除全部訂單");
+			getOrderData();
+		});
+});
+
+
+//C3
+function createProductData() {
+	productName = {};
+	orderData.forEach((item) => {
+		item.products.forEach((item) => {
+			if (productName[item.title] == undefined) {
+				productName[item.title] = item.quantity * item.price;
+			} else {
+				productName[item.title] += item.quantity * item.price;
+			}
+		});
+	});
+}
+
+function creatC3Data() {
+	c3Data = [];
+	let product = Object.keys(productName);
+	let j;
+	let str = "";
+	let num;
+	//sort products
+	product.forEach((item, index) => {
+		num = productName[item];
+		for (let i = index; i < product.length; i++) {
+			if (num < productName[product[i]]) {
+				num = productName[product[i]];
+				str = item;
+				j = i;
+			}
+		}
+		if (productName[item] < num) {
+			product[index] = product[j];
+			product[j] = str;
+		}
+	});
+
+	if (product.length >= 4) {
+		let others = 0;
+		for (let i = 3; i < product.length; i++) {
+			others += productName[product[i]];
+			delete productName[product[i]];
+		}
+		product.splice(3, product.length);
+		product.push("其他");
+		productName["其他"] = others;
+	}
+
+	product.forEach((item) => {
+		let aryC3 = [];
+		aryC3.push(item);
+		aryC3.push(productName[item]);
+		c3Data.push(aryC3);
+	});
+}
+
+function renderC3() {
+	let colorArray = {};
+	//set colors
+	c3Data.forEach((item, index) => {
+		colorArray[item[0]] = colors[index];
+	});
+
+	const chart = c3.generate({
+		bindo: "#chart",
+		data: {
+			columns: c3Data,
+			type: "pie",
+			colors:	colorArray
+		},
+		size: {
+			width: 1100,
+			height: 350
+		},
+	});
+}
+
+function init() {
+	getOrderData();
+}
+
+init();
---
+++ script.js
@@ -0,0 +1,241 @@
+let generalURL = "https://hexschoollivejs.herokuapp.com/api/livejs/v1";
+let api_path = "hahaeating";
+let productData = [];
+let cartData = [];
+
+let recommendList1 = document.querySelector(".recommendList");
+let recommendList2 = document.querySelector(".recommendList2");
+let productList = document.querySelector(".productList");
+let productSelect = document.querySelector("#productSelect");
+let shopList = document.querySelector(".shopList");
+let amountPrice = document.querySelector(".delete span");
+let deleteAll = document.querySelector(".deleteAll");
+let orderInfo = document.querySelector(".orderInfo");
+
+let recommendDataRow1 = [
+	{
+		img: "./img/recommend/recommendImg1.png",
+		head: "./img/recommend/recommendHead1.png",
+		name: "王六角",
+		item: "Jordan雙人床架",
+		comment: "CP值很高。"
+	},
+	{
+		img: "./img/recommend/recommendImg2.png",
+		head: "./img/recommend/recommendHead2.png",
+		name: "Leaf",
+		item: "Antony雙人床架",
+		comment: "很喜歡～還有送三年保固～"
+	},
+	{
+		img: "./img/recommend/recommendImg3.png",
+		head: "./img/recommend/recommendHead3.png",
+		name: "美濃鄧子琪",
+		item: "Charles系列廚",
+		comment: "廚房必備美用品！"
+	}
+];
+let recommendDataRow2 = [
+	{
+		img: "./img/recommend/recommendImg4.png",
+		head: "./img/recommend/recommendHead4.png",
+		name: "小杰",
+		item: "Louvre雙人床架",
+		comment: "非常舒適，有需要會再回購"
+	},
+	{
+		img: "./img/recommend/recommendImg5.png",
+		head: "./img/recommend/recommendHead5.png",
+		name: "江六角",
+		item: "Charles雙人床架",
+		comment: "品質不錯～"
+	},
+	{
+		img: "./img/recommend/recommendImg6.png",
+		head: "./img/recommend/recommendHead6.png",
+		name: "juni讚神",
+		item: "Antony床邊桌",
+		comment: "讚ㄉ！"
+	}
+];
+
+//recommend part
+function recommendDisplay(row, list) {
+	row.forEach((item) => {
+		list.innerHTML += `
+		<div class="recommendEach">
+			<img src=${item.img} alt="">
+			<div class="recommendDetail">
+				<div class="recommendName">
+					<img src=${item.head} alt="">
+					<div>
+						<p>${item.name}</p>
+						<p>${item.item}</p>
+					</div>
+				</div>
+				<p>${item.comment}</p>
+			</div>
+		</div>
+		`;
+	});
+}
+recommendDisplay(recommendDataRow1, recommendList1);
+recommendDisplay(recommendDataRow2, recommendList2);
+
+function getProductData() {
+	axios.get(`${generalURL}/customer/${api_path}/products`)
+		.then(function (response) {
+			productData = response.data.products;
+			renderProduct(productData);
+		});
+}
+
+//product part
+function renderProduct(data) {
+	let str = "";
+	data.forEach((item) => {
+		str += `
+		<div class="productEach">
+			<div class="productDetail">
+				<div class="productImg">
+					<img src=${item.images} alt="">
+					<p class="addToCart" data-id="${item.id}">加入購物車</p>
+					<p class="newProduct">新品</p>
+				</div>
+				<p>${item.title}</p>
+				<p><s>NT$<span>${item.origin_price}</span></s></p>
+				<p>NT$<span>${item.price}</span></p>
+			</div>
+		</div>
+		`;
+	});
+	productList.innerHTML = str;
+}
+
+//select product by category
+productSelect.addEventListener("change", function() {
+	let selectProduct = [];
+	productData.forEach((item) => {
+		if (productSelect.value == item.category) {
+			selectProduct.push(item);
+		} else if (productSelect.value == "全部") {
+			selectProduct = productData;
+		}
+	});
+	renderProduct(selectProduct);
+});
+
+//Cart data display
+function getCartData() {
+	axios.get(`${generalURL}/customer/${api_path}/carts`)
+		.then(function (response) {
+			cartData = response.data;
+			renderCart();
+		});
+}
+
+//cart part
+function renderCart() {
+	let str = "";
+	cartData.carts.forEach((item) => {
+		str += `
+		<div class="shopEach">
+			<div>
+				<img src=${item.product.images} alt="">
+				<p>${item.product.title}</p>
+			</div>
+			<p>NT$<span>${item.product.price}</span></p>
+			<p>${item.quantity}</p>
+			<p>NT$<span>${item.product.price * item.quantity}</span></p>
+			<i class="las la-times" data-id="${item.id}"></i>
+		</div>
+		`;
+	});
+	shopList.innerHTML = str;
+	amountPrice.innerHTML = `NT$${cartData.finalTotal}`;
+}
+
+//add to cart
+productList.addEventListener("click", function(e) {
+	const productID = e.target.getAttribute("data-id");
+	let num = 0;
+	cartData.carts.forEach((item) => {
+		if (productID == item.product.id) {
+			num = item.quantity;
+		}
+	});
+	if (e.target.getAttribute("class") == "addToCart"){
+		axios.post(`${generalURL}/customer/${api_path}/carts`, {
+			"data": {
+				"productId": `${productID}`,
+				"quantity": num+1
+			}
+		})
+			.then(function () {
+				getCartData();
+
+			});
+	}
+});
+
+//delete a item
+shopList.addEventListener("click", function(e) {
+	let cartID = e.target.getAttribute("data-id");
+	console.log(cartID);
+	axios.delete(`${generalURL}/customer/${api_path}/carts/${cartID}`)
+		.then(function () {
+			getCartData();
+		});
+});
+
+//delete all items
+deleteAll.addEventListener("click", function() {
+	axios.delete(`${generalURL}/customer/${api_path}/carts`)
+		.then(function () {
+			getCartData();
+		});
+});
+
+//order
+orderInfo.addEventListener("click", function(e) {
+	let name = document.querySelector("#name");
+	let tel = document.querySelector("#tel");
+	let email = document.querySelector("#email");
+	let address = document.querySelector("#address");
+	let tradeMethod = document.querySelector("#tradeMethod");
+
+	if (e.target.value == "送出預訂資料") {
+		if ( name.value == "" && tel.value == "" &&
+			email.value == "" && address.value == "") {
+			alert("資訊不得為空");
+			return;
+		} else {
+			axios.post(`${generalURL}/customer/${api_path}/orders`, {
+				"data": {
+					"user": {
+						"name": `${name.value}`,
+						"tel": `${tel.value}`,
+						"email": `${email.value}`,
+						"address": `${address.value}`,
+						"payment": `${tradeMethod.value}`
+					}
+				}
+			})
+				.then(function () {
+					getCartData();
+					alert("訂單預定成功！");
+					name.value = "";
+					tel.value = "";
+					email.value = "";
+					address.value = "";
+				});
+		}
+	}
+});
+
+function init() {
+	getProductData();
+	getCartData();
+}
+
+init();
